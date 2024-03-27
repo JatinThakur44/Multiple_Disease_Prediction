f8e9f9b674d0fc4d6adb6eb3e5232a8149df0969
JatinThakur44 <128354764+JatinThakur44@users.noreply.github.com>
2024-03-27 09:51:06 +0530

    Update multiple_disease_prediction.py.py

--- multiple_disease_prediction.py.py
+++ multiple_disease_prediction.py.py
@@ -1,67 +1,52 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue May 16 00:36:03 2023
-
-@author: jatin
-"""
-
+import os
 import pickle
 import streamlit as st
 from streamlit_option_menu import option_menu
 
-# loading the saved models
-diabetes_model = pickle.load(
-    open('C:/Users/jatin/PycharmProjects/Multiple Disease Prediction/mdp/diabites_model.sav', 'rb'))
+# Get the directory of the current script
+current_dir = os.path.dirname(__file__)
 
-heart_disease_model = pickle.load(
-    open('C:/Users/jatin/PycharmProjects/Multiple Disease Prediction/mdp/heart_model.sav', 'rb'))
+# Define file paths relative to the current directory
+diabetes_model_path = os.path.join(current_dir, 'mdp', 'diabites_model.sav')
+heart_disease_model_path = os.path.join(current_dir, 'mdp', 'heart_model.sav')
+kidney_model_path = os.path.join(current_dir, 'mdp', 'kidney_model.sav')
 
-kidney_model = pickle.load(
-    open('C:/Users/jatin/PycharmProjects/Multiple Disease Prediction/mdp/kidney_model.sav', 'rb'))
+# Load the saved models
+diabetes_model = pickle.load(open(diabetes_model_path, 'rb'))
+heart_disease_model = pickle.load(open(heart_disease_model_path, 'rb'))
+kidney_model = pickle.load(open(kidney_model_path, 'rb'))
 
-# side barr for navigation
+# Side bar for navigation
 with st.sidebar:
     selected = option_menu('Multiple disease prediction system',
-
-                           ['Diabetes Prediction',
-                            'Heart disease prediction',
-                            'Kidney disease prediction'],
-
+                           ['Diabetes Prediction', 'Heart disease prediction', 'Kidney disease prediction'],
                            icons=['activity', 'heart', 'person'],
                            default_index=0)
 
-# diabetes prediction page
+# Diabetes prediction page
 if selected == "Diabetes Prediction":
-    # page title
+    # Page title
     st.title('Diabetes Prediction using ML')
 
-    # getting the input data from the user
-    # columns for the input fields
+    # Input fields
     col1, col2, col3 = st.columns(3)
-
     with col1:
         Pregnancies = st.text_input("Number of Pregnancies")
-    with col2:
-        Glucose = st.text_input("Glucose level")
-    with col3:
-        BloodPressure = st.text_input("Blood Pressure Value")
-    with col1:
         SkinThickness = st.text_input("Skin Thickness Value")
-    with col2:
-        Insulin = st.text_input("Insulin Level")
-    with col3:
-        BMI = st.text_input("BMI value")
-    with col1:
         DiabetesPedigreeFunction = st.text_input("Diabetes Pedigree Function value")
     with col2:
+        Glucose = st.text_input("Glucose level")
+        Insulin = st.text_input("Insulin Level")
+        BMI = st.text_input("BMI value")
+    with col3:
+        BloodPressure = st.text_input("Blood Pressure Value")
         Age = st.text_input("Age of the person")
 
-    # code for prediction
+    # Prediction
     diab_diagnosis = ''
-    # creating a button for prediction
     if st.button("Diabetes Test Results"):
-        diab_prediction = diabetes_model.predict(
-            [[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age]])
+        diab_prediction = diabetes_model.predict([[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin,
+                                                    BMI, DiabetesPedigreeFunction, Age]])
 
         if diab_prediction[0] == 1:
             diab_diagnosis = "The Person is Diabetic"
@@ -69,42 +54,29 @@ if selected == "Diabetes Prediction":
             diab_diagnosis = "The Person is Not Diabetic"
     st.success(diab_diagnosis)
 
+# Heart disease prediction
 if selected == "Heart disease prediction":
-
     st.title('Heart disease Prediction using ML')
 
     col1, col2, col3 = st.columns(3)
-
     with col1:
         age = st.text_input("Age of the Person")
+        chestpain = st.text_input("Chest Pain Value")
+        fastingbloodsugar = st.text_input("Fasting Blood Sugar value")
+        restecg = st.text_input("Restecg value")
+        exang = st.text_input("Exang value")
+        slope = st.text_input("Slope value")
+        thal = st.text_input("Thal value")
     with col2:
         sex = st.text_input("Male or Female")
-    with col3:
-        chestpain = st.text_input("Chest Pain Value")
-    with col1:
         trestbps = st.text_input("Trestbps Value")
-    with col2:
         cholestoral = st.text_input("Cholestoral Level")
-    with col3:
-        fastingbloodsugar = st.text_input("Fasting Blood Sugar value")
-    with col1:
-        restecg = st.text_input("Restecg value")
-    with col2:
         thalach = st.text_input("Thalach value")
-    with col3:
-        exang = st.text_input("Exang value")
-    with col1:
         oldpeak = st.text_input("OldPeak value")
-    with col2:
-        slope = st.text_input("Slope value")
-    with col3:
         ca = st.text_input("Ca value")
-    with col1:
-        thal = st.text_input("Thal value")
 
-    # code for prediction
+    # Prediction
     heart_diagnosis = ''
-    # creating a button for prediction
     if st.button("Heart Disease Test Results"):
         heart_prediction = heart_disease_model.predict([[age, sex, chestpain, trestbps, cholestoral, fastingbloodsugar,
                                                          restecg, thalach, exang, oldpeak, slope, ca, thal]])
@@ -115,41 +87,30 @@ if selected == "Heart disease prediction":
             heart_diagnosis = "The Person is Not having heart disease"
     st.success(heart_diagnosis)
 
+# Kidney disease prediction
 if selected == "Kidney disease prediction":
     st.title('Kidney disease Prediction using ML')
 
     col1, col2, col3 = st.columns(3)
-
     with col1:
         Bp = st.text_input("BP value")
+        Al = st.text_input("Al Value")
+        Rbc = st.text_input("RBC value")
+        Sc = st.text_input("Sc value")
+        Pot = st.text_input("Pot value")
+        Wbcc = st.text_input("Wbcc value")
     with col2:
         Sg = st.text_input("Sg value")
-    with col3:
-        Al = st.text_input("Al Value")
-    with col1:
         Su = st.text_input("SU Value")
-    with col2:
-        Rbc = st.text_input("RBC value")
-    with col3:
         Bu = st.text_input("BU value")
-    with col1:
-        Sc = st.text_input("Sc value")
-    with col2:
         Sod = st.text_input("Sod value")
-    with col3:
-        Pot = st.text_input("Pot value")
-    with col1:
         Hemo = st.text_input("Hemo value")
-    with col2:
-        Wbcc = st.text_input("Wbcc value")
-    with col3:
         Rbcc = st.text_input("Rbcc value")
-    with col1:
+    with col3:
         Htn = st.text_input("Htn value")
 
-    # code for prediction
+    # Prediction
     kidney_diagnosis = ''
-    # creating a button for prediction
     if st.button("Kidney Disease Test Results"):
         kidney_prediction = kidney_model.predict([[Bp, Sg, Al, Su, Rbc, Bu, Sc, Sod, Pot, Hemo, Wbcc, Rbcc, Htn]])
 
